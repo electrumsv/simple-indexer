fdbf26e73e2df8222ec738806f52e0049306d117
AustEcon <AustEcon0922@gmail.com>
2021-11-12 20:38:29 +1300

    Add a binary api for pushdata matches the urls are as follows:
    - get_pushdata_filter_matches.json for JSON
    - get_pushdata_filter_matches.bin for Binary response
    - the block height is now included in the response
    - the Flags are now a 1 byte char with room to include information such as "out of funds" and terminating the stream.

--- simple_indexer/handlers.py
+++ simple_indexer/handlers.py
@@ -1,11 +1,13 @@
 import json
+import struct
 import typing
 
 import aiohttp
 from aiohttp import web
 import logging
 
-from simple_indexer.types import RestorationFilterRequest
+from simple_indexer.types import RestorationFilterRequest, RESULT_UNPACK_FORMAT, \
+    filter_response_struct, FILTER_RESPONSE_SIZE
 
 if typing.TYPE_CHECKING:
     from simple_indexer.server import ApplicationState
@@ -23,7 +25,7 @@ async def error(request: web.Request) -> web.Response:
     raise ValueError("This is a test of raising an exception in the handler")
 
 
-async def get_pushdata_filter_matches(request: web.Request):
+async def get_pushdata_filter_matches_json(request: web.Request):
     """This the main endpoint for the rapid restoration API"""
     app_state: 'ApplicationState' = request.app['app_state']
     sqlite_db: SQLiteDatabase = app_state.sqlite_db
@@ -40,10 +42,40 @@ async def get_pushdata_filter_matches(request: web.Request):
     response = aiohttp.web.StreamResponse(status=200, reason='OK', headers=headers)
     await response.prepare(request)
 
-    result = sqlite_db.get_pushdata_filter_matches(pushdata_hashes)
+    result = sqlite_db.get_pushdata_filter_matches(pushdata_hashes, json=True)
     for match in result:
         row = (json.dumps(match) + "\n").encode('utf-8')
         await response.write(row)
 
+    await response.write_eof()
+    return response
+
+
+async def get_pushdata_filter_matches_binary(request: web.Request):
+    """This the main endpoint for the rapid restoration API"""
+    app_state: 'ApplicationState' = request.app['app_state']
+    sqlite_db: SQLiteDatabase = app_state.sqlite_db
+    body = await request.content.read()
+    if body:
+        pushdata_hashes: RestorationFilterRequest = json.loads(body.decode('utf-8'))['filterKeys']
+    else:
+        return web.json_response({'error': 'no pushdata hashes submitted in request body'})
+
+    headers = {
+        'Content-Type': 'application/octet-stream',
+        'User-Agent': 'SimpleIndexer'
+    }
+    response = aiohttp.web.StreamResponse(status=200, reason='OK', headers=headers)
+    await response.prepare(request)
+
+    result = sqlite_db.get_pushdata_filter_matches(pushdata_hashes, json=False)
+    count = 0
+    for match in result:
+        packed_match = filter_response_struct.pack(*match)
+        await response.write(packed_match)
+        count += 1
+
+    total_size = count * FILTER_RESPONSE_SIZE
+    logger.debug(f"Total pushdata filter match response size: {total_size} for count: {count}")
     await response.write_eof()
     return response
--- simple_indexer/types.py
+++ simple_indexer/types.py
@@ -1,14 +1,38 @@
+import struct
 import typing
+from typing import TypedDict, Optional, NamedTuple
 
 
 class RestorationFilterRequest(typing.TypedDict):
     filterKeys: typing.List[str]
 
 
-class RestorationFilterResponse(typing.TypedDict):
+class RestorationFilterJSONResponse(TypedDict):
+    flags: int
     pushDataHashHex: str
     transactionId: str
     index: int
-    referenceType: int
-    spendTransactionId: typing.Optional[str]
+    spendTransactionId: Optional[str]
     spendInputIndex: int
+    blockHeight: int
+
+
+class RestorationFilterResult(NamedTuple):
+    flags: bytes
+    push_data_hash: bytes
+    transaction_hash: bytes
+    spend_transaction_hash: bytes
+    transaction_output_index: int
+    spend_input_index: int
+    block_height: int
+
+
+RESULT_UNPACK_FORMAT = ">c32s32s32sIII"
+FILTER_RESPONSE_SIZE = 1 + 32 + 32 + 32 + 4 + 4 + 4
+assert struct.calcsize(RESULT_UNPACK_FORMAT) == FILTER_RESPONSE_SIZE
+
+filter_response_struct = struct.Struct(RESULT_UNPACK_FORMAT)
+
+
+def le_int_to_char(le_int):
+    return struct.pack('<I', le_int)[0:1]
